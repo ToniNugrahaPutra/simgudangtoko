8ce20e374423907bd3c72d7fdc136e6f96d64380
Fajrulll <123521836+buyung1610@users.noreply.github.com>
2026-01-07 13:59:48 +0700

    update

--- app/Http/Requests/TransaksiRequest.php
+++ app/Http/Requests/TransaksiRequest.php
@@ -22,21 +22,12 @@ class TransaksiRequest extends FormRequest
     public function rules(): array
     {
         return [
-<<<<<<< HEAD
-            'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:20',
-            'merchant_id' => 'required|exists:merchants,id',
-            'products' => 'required|array|min:1',
-            'products.*.product_id' => 'required|exists:products,id',
-            'products.*.quantity' => 'required|integer|min:1',
-=======
             'nama' => 'required|string|max:255',
             'no_hp' => 'required|string|max:20',
             'toko_id' => 'required|exists:merchants,id',
             'produk' => 'required|array|min:1',
             'produk.*.produk_id' => 'required|exists:produk,id',
             'produk.*.jumlah' => 'required|integer|min:1',
->>>>>>> 0727bfbe7bd8a4de8d7e1c6d3533072f96ca7ff6
         ];
     }
 }
