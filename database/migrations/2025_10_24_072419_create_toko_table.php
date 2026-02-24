9bc557d44cdba286cef692cbf1298349b74569f3
cahyaa <sadi35543@gmail.com>
2026-02-24 15:49:26 +0700

    update : fix formatting and constraints in toko migration

--- database/migrations/2025_10_24_072419_create_toko_table.php
+++ database/migrations/2025_10_24_072419_create_toko_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,11 @@ return new class extends Migration
     {
         Schema::create('toko', function (Blueprint $table) {
             $table->id();
-            $table->string('nama')-> unique();
+            $table->string('nama')->unique();
             $table->text('alamat');
             $table->string('foto');
-            $table->string('no_hp')-> unique();
-            $table->foreignId('operator_id')->constrained('users')->onDelete('cascade');
+            $table->string('no_hp')->unique();
+            $table->foreignId('operator_id')->constrained('pengguna')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
